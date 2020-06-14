aa021108c0aa5768c92f1a7fe54b5b8b0fe09499
techmiya-hub <miya1123miya@gmail.com>
2020-06-14 10:14:49 +0900

    Refactoring MemoList

--- App.js
+++ App.js
@@ -1,58 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-import BodyText from './src/elements/BodyText'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import MemoList from './src/components/MemoList'
+import Appbar from './src/components/Appbar'
+import CircleButton from './src/elements/CircleButton'
 
 export default function App() {
   return (
     <View style={styles.container}>
-
-      <View style={styles.appbar}>
-        <View>
-          <Text style={styles.appbarTitle}>MEMOT</Text>
-        </View>
-      </View>
-
-      <View style={styles.memoList}>
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>講座のアイテム</Text>
-          <Text style={styles.momoDate}>2020/01/01</Text>
-        </View>
-      </View>
-
-      <View style={styles.memoList}>
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>講座のアイテム</Text>
-          <Text style={styles.momoDate}>2020/01/01</Text>
-        </View>
-      </View>
-
-      <View style={styles.memoList}>
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>講座のアイテム</Text>
-          <Text style={styles.momoDate}>2020/01/01</Text>
-        </View>
-      </View>
-
-      <View style={styles.memoList}>
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>講座のアイテム</Text>
-          <Text style={styles.momoDate}>2020/01/01</Text>
-        </View>
-      </View>
-
-      <View style={styles.memoList}>
-        <View style={styles.memoListItem}>
-          <Text style={styles.memoTitle}>講座のアイテム</Text>
-          <Text style={styles.momoDate}>2020/01/01</Text>
-        </View>
-      </View>
-
-      <View style={styles.memoAddButton}>
-        <Text style={styles.memoAddButtonTitle}>+</Text>
-      </View>
+      <Appbar />
+      <MemoList />
+      <CircleButton>+</CircleButton>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -63,63 +22,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: 78,
   },
-  memoAddButton: {
-    position: 'absolute',
-    bottom: 32,
-    right: 32,
-    width: 48,
-    height: 48,
-    backgroundColor: '#E31676',
-    borderRadius: 24,
-    justifyContent: "center",
-    alignItems: "center",
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 0},
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    zIndex: 10,
-  },
-  memoAddButtonTitle: {
-    fontSize: 32,
-    lineHeight: 32,
-    color: '#fff',
-  },
-  memoList: {
-    width: '100%',
-    // flex: 1,
-  },
-  memoListItem: {
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-    backgroundColor: '#fff',
-  },
-  memoTitle: {
-    fontSize: 18,
-    marginBottom: 4,
-  },
-  momoDate: {
-    fontSize: 12,
-    color: '#a2a2a2'
-  },
-  appbar: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    height: 78,
-    paddingTop: 30,
-    backgroundColor: '#265366',
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 0},
-    shadowOpacity: 0.9,
-    shadowRadius: 3,
-    zIndex: 10,
-  },
-  appbarTitle: {
-    color: '#fff',
-    fontSize: 18,
-  }
-});
+})
---
+++ src/components/MemoList.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+class MemoList extends React.Component {
+  render() {
+    return (
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.momoDate}>2020/01/01</Text>
+        </View>        
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.momoDate}>2020/01/01</Text>
+        </View>        
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.momoDate}>2020/01/01</Text>
+        </View>        
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.momoDate}>2020/01/01</Text>
+        </View>        
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.momoDate}>2020/01/01</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  memoList: {
+    flex: 1,
+    width: '100%',
+  },
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    backgroundColor: '#fff',
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+  momoDate: {
+    fontSize: 12,
+    color: '#a2a2a2'
+  },
+})
+
+export default MemoList
